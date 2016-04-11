000286d9ed321696a5b3c867ab69e443fd65dc02
javi <jaanga@live.com>
2016-04-12 00:45:51 +0200

    formulario que recibe parametros por get JAVA EE

---
+++ src/com/prueba/envio.java
@@ -0,0 +1,33 @@
+package com.prueba;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.PrintWriter;
+
+public class envio extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String user = req.getParameter("user");
+		String pass = req.getParameter("password");
+		if ("restyu".equals(user) && "1234".equals(pass)) {
+			response(resp, "login ok");
+		} else {
+			response(resp, "invalid login");
+		}
+	}
+
+	private void response(HttpServletResponse resp, String msg)
+			throws IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<t1>" + msg + "</t1>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+}
